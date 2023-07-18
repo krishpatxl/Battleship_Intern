ce4fc13b5c0aa9988b0c0852c763c9ff91a1ceaf
krishpatxl <138689814+krishpatxl@users.noreply.github.com>
2023-07-18 10:23:42 -0400

    Updated battleship.py

--- battleship.py
+++ battleship.py
@@ -1,3 +1,31 @@
+from random import randint
+
+Hidden_Pattern=[['0']*7 for x in range(7)]
+Guess_Pattern=[['0']*7 for x in range(7)]
+
+def print_board(board):
+    print('  A B C D E F G')
+ 
+    row_num=1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num +=1
+
+abc = ["A","B","C","D","E","F","G"]
+def get_ship_location():
+    #Enter the row number between 1 to 7
+
+    row = input('Please enter a ship row 1-7 ')
+    while row not in '1234567':
+        print("Please enter a valid row ")
+        row=input('Please enter a ship row 1-7 ')
+    #Enter the Ship column from A TO G
+    column = input('Please enter a ship column A-G ').upper()
+    
+    while column not in abc:
+        print("Please enter a valid column ")
+        column=input('Please enter a ship column A-G ')
+    return int(row)-1,let_to_num[column]
 # starts at line 33
 # This function creates the ships
 def create_ships(board):
@@ -17,37 +45,42 @@ def count_hit_ships(board):
                 count+=1
     return count
 while True:
-    a = input("Would you like to play by yourself?")
-    if a == " No" or a == " No":
-        exit
-    print('Welcome to Battleship, Lets Play !!') 
-    Hidden_Pattern=[[' ']*7 for x in range(7)]
-    Guess_Pattern=[[' ']*7 for x in range(7)]
+    a = input("How do you want the ships placed?")
+    if a == " Myself" or a == " by myself" or a == " By Myself" or a == " By myself":
+        break
+    else:
+        print('Welcome to Battleship, Lets Play !!') 
+        Hidden_Pattern=[['0']*7 for x in range(7)]
+        Guess_Pattern=[['0']*7 for x in range(7)]
 
-    let_to_num={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
-    create_ships(Hidden_Pattern)
-    #print_board(Hidden_Pattern)
-    turns = 2
-    while turns > 0:
-        print_board(Guess_Pattern)
-        row,column =get_ship_location()
-        if Guess_Pattern[row][column] == 'O':
-            print(' You already guessed that, try again. ')
-        elif Hidden_Pattern[row][column] =='X':
-            print(' Congrats, you have hit a ship ')
-            Guess_Pattern[row][column] = 'O'
-            turns -= 1
-        else:
-            print('You missed LOL')
-            Guess_Pattern[row][column] = 'x'
-            turns -= 1
-        if  count_hit_ships(Guess_Pattern) == 7:
-            print("You hit all the ships")
+        let_to_num={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
+        create_ships(Hidden_Pattern)
+        #print_board(Hidden_Pattern)
+        turns = 10
+        while turns > 0:
+            print_board(Guess_Pattern)
+            row,column =get_ship_location()
+            if Guess_Pattern[row][column] == 'O':
+                print(' You already guessed that, try again. ')
+            elif Hidden_Pattern[row][column] =='X':
+                print ("You hit " + str(row+1) +' ' + abc[column])
+                print(' Congrats, you have hit a ship ')
+                Guess_Pattern[row][column] = 'O'
+                turns -= 1
+            else:
+                print ("You hit " + str(row+1) +' ' + abc[column])
+                print('You missed LOL')
+                Guess_Pattern[row][column] = 'x'
+                turns -= 1
+            if  count_hit_ships(Guess_Pattern) == 7:
+                print("You hit all the ships")
+                break
+            print(' You have ' +str(turns) + ' tries left ')
+            if turns == 0:
+                print('You lost')
+                break 
+        a = input("Would you like to play again?")
+        if a == " No" or a == " no":
             break
-        print(' You have ' +str(turns) + ' tries left ')
-        if turns == 0:
-            print('You lost')
-            break 
-    a = input("Would you like to play again?")
-    if a == " No" or a == " no":
-       break
+    
+    
