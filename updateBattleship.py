153f877df6c46a831e009c67b98e322bb285586c
krishpatxl <138689814+krishpatxl@users.noreply.github.com>
2023-07-19 11:57:37 -0400

    Update and rename battleship.py to updateBattleship.py

--- updateBattleship.py
+++ updateBattleship.py
@@ -1,10 +1,10 @@
 from random import randint
 from string import ascii_uppercase as letters
 
-
+#Creating the board
 def create_board():
     try:
-        size = eval(input("Please enter a size: "))
+        size = eval(input("Please enter a size:"))
     except:
         print("That is not a number, try again")
 
@@ -12,46 +12,42 @@ def create_board():
     Guess_Pattern=[[' ']*size for x in range(size)]
 
     return Hidden_Pattern, Guess_Pattern, size
-
+#Printing the board together
 def print_board(board):
     row_num=1
     for row in board:
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num +=1
 
-abc = ["A","B","C","D","E","F","G"]
+abc = ["A","B","C","D","E","F","G","H","I","J"]
 def get_ship_location():
-    #Enter the row number between 1 to 7
-
-    row = input('Please enter a ship row 1-7 ')
-    while row not in '1234567':
+    #Enter the row number between 1 to 10
+    row = (input('Please enter a ship row: '))
+    while row not in '12345678910':
         print("Please enter a valid row ")
-        row=input('Please enter a ship row 1-7 ')
-    #Enter the Ship column from A TO G
-    column = input('Please enter a ship column A-G ').upper()
-    
+        row=input('Please enter a ship row: ')
+    #Enter the Ship column from A TO J
+    column = input('Please enter a ship column: ').upper()
+
     while column not in abc:
         print("Please enter a valid column ")
-        column=input('Please enter a ship column A-G ')
+        column=input('Please enter a ship column A-J ').upper()
     return int(row)-1,let_to_num[column]
-# starts at line 33
-# This function creates the ships
+#starts at line 33
+#This function creates the ships
 def create_ships(board):
     for ship in range(size):
-        ship_r, ship_cl=randint(0,size-1), randint(0,size-1)
+        ship_r, ship_cl=randint(0,size-1), randint(0, size-1)
         while board[ship_r][ship_cl] =='X':
             ship_r, ship_cl = randint(0, size-1), randint(0, size-1)
         board[ship_r][ship_cl] = 'X'
-
 def input_validation(row, column, size):
-    
-    if row not in range(size):
-        print("please enter a number on the board")
-    
-    if column not in range (size):
-        print("please enter a number on the board")
-        
 
+    if row not in range(size):
+        print("Please enter a number on the board")
+
+    if column not in range (size):
+        print("Please enter a number on the board")
 def count_hit_ships(board):
     count=0
     for row in board:
@@ -60,19 +56,21 @@ def count_hit_ships(board):
                 count+=1
     return count
 while True:
+    print('Welcome to Battleship, Lets Play !!') 
+    Hidden_Pattern, Guess_Pattern, size = create_board()
+    
     a = input("Do you want the ships placed by yourself? ")
     if a == "No" or a == "no":
-        print('Welcome to Battleship, Lets Play !!') 
-        Hidden_Pattern, Guess_Pattern, size = create_board()
-
-        let_to_num={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
-        create_ships(Hidden_Pattern)
-        #print_board(Hidden_Pattern)
         
-        turns = 10
+
+        let_to_num={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9}
+        #create_ships(Hidden_Pattern)
+        #print_board(Hidden_Pattern)
+
+        turns = 36
         while turns > 0:
             print_board(Guess_Pattern)
-            row,column =get_ship_location()
+            row,column = get_ship_location()
             if Guess_Pattern[row][column] == 'O':
                 print(' You already guessed that, try again. ')
             elif Hidden_Pattern[row][column] =='X':
@@ -95,3 +93,11 @@ while True:
         a = input("Would you like to play again?")
         if a == " No" or a == " no":
             break
+        
+    else: 
+        a == "Yes" or a =="yes"
+        row=input('Please enter a ship row: ')
+        while row not in '12345678910':
+            print("Please enter a valid row ")
+        column=input("Please enter a ship column: ")
+        print_board(Hidden_Pattern)
