1dc0d71bc455648821d02c36ee43deb9ac6e76fe
krishpatxl <138689814+krishpatxl@users.noreply.github.com>
2023-07-21 09:47:33 -0400

    Update and rename fullUpdateButBrokeCode.py to BtlShip2.1code.py

--- BtlShip2.1code.py
+++ BtlShip2.1code.py
@@ -1,31 +1,17 @@
 import random
 
-# Function to create an empty grid of given size
 def create_grid(size):
     return [['O' for _ in range(size)] for _ in range(size)]
 
-# Function to print the user's board
-def print_user_board(grid, size):
+def print_board(grid, size):
     for row in grid:
         print(' '.join('X' if col == 'U' else col for col in row))
 
-# Function to place a ship on the grid
-def place_ship(grid, name, length, size):
-    while True:
-        try:
-            row = int(input(f"Enter the row index for the {name} (1 to {size}): "))
-            col = int(input(f"Enter the column index for the {name} (1 to {size}): "))
-            if 1 <= row <= size and 1 <= col <= size:
-                row -= 1
-                col -= 1
-                break
-            else:
-                print("Invalid input. Try again.")
-        except ValueError:
-            print("Invalid input. Please enter valid row and column numbers.")
-
-    # Check if the ship fits on the grid
-    if col + length > size:
+def place_ship(grid, name, length, size, row, col):
+    if 1 <= row <= size and 1 <= col + length <= size:
+        row -= 1
+        col -= 1
+    else:
         print("Invalid ship placement. Ship does not fit on the grid.")
         return False
 
@@ -39,14 +25,31 @@ def place_ship(grid, name, length, size):
 
     return True
 
-# Function to check if the guess is valid
+def randomly_place_ships(grid, num_ships, size):
+    for i in range(num_ships):
+        name = f"Ship{i + 1}"
+        while True:
+            length = 1  # 1x1 ships
+            row = random.randint(1, size)
+            col = random.randint(1, size)
+            if place_ship(grid, name, length, size, row, col):
+                break
+
+def computer_guess(user_ships, size):
+    while True:
+        guess_row = random.randint(0, size - 1)
+        guess_col = random.randint(0, size - 1)
+        guess = (guess_row, guess_col)
+
+        if guess not in user_ships:
+            return guess
+
 def is_valid_guess(guess, size):
     if len(guess) != 2:
         return False
     row, col = guess
     return 0 <= row < size and 0 <= col < size
 
-# Function to check if the guess hit a ship and if it was sunk
 def check_hit_and_sunk(guess, ships):
     hit_ship = None
     hit_ship_index = None
@@ -67,7 +70,6 @@ def check_hit_and_sunk(guess, ships):
     else:
         return "miss", None
 
-# Main function to run the game
 def battleship_game():
     print("Welcome to Battleship!")
 
@@ -85,33 +87,30 @@ def battleship_game():
     opponent_grid = create_grid(size)
 
     print("Your board:")
-    print_user_board(user_grid, size)
+    print_board(user_grid, size)
+
+    place_option = input("Do you want to place your ship manually? (yes/no): ").lower()
 
     num_ships = int(input("Enter the number of ships you want to place: "))
-    user_ships = []
 
-    for i in range(num_ships):
-        name = input(f"Enter the name for Ship {i + 1}: ")
-        while True:
-            length = 2  # 2x1 ships
-            if place_ship(user_grid, name, length, size):
-                user_ships.append([(row, col + i) for i in range(length)])
-                break
+    if place_option == 'yes':
+        for i in range(num_ships):
+            name = input(f"Enter the name for Ship {i + 1}: ")
+            while True:
+                length = 1  # 1x1 ships
+                row = int(input(f"Enter the row index for the {name} (1 to {size}): "))
+                col = int(input(f"Enter the column index for the {name} (1 to {size}): "))
+                if place_ship(user_grid, name, length, size, row, col):
+                    break
+    else:
+        randomly_place_ships(user_grid, num_ships, size)
 
-    opponent_ships = []
-    num_opponent_ships = num_ships
-
-    for i in range(num_opponent_ships):
-        name = f"Ship{i + 1}"
-        while True:
-            length = 2  # 2x1 ships
-            if place_ship(opponent_grid, name, length, size):
-                opponent_ships.append([(row, col + i) for i in range(length)])
-                break
+    randomly_place_ships(opponent_grid, num_ships, size)
+    user_ships = {(i, j) for i in range(size) for j in range(size) if user_grid[i][j] != 'O'}
 
     user_guesses = []
     opponent_guesses = []
-    max_attempts = 8
+    max_attempts = 5
 
     for attempt in range(max_attempts):
         print(f"\nAttempt {attempt + 1}: Your Turn")
@@ -137,49 +136,46 @@ def battleship_game():
 
         user_guesses.append(guess)
 
-        result, sunk_index = check_hit_and_sunk(guess, opponent_ships)
+        result, sunk_index = check_hit_and_sunk(guess, opponent_grid)
 
         if result == "sunk":
-            print(f"Congratulations! You sunk the opponent's {opponent_ships[sunk_index][0][0]}!")
+            print(f"Congratulations! You sunk the opponent's {opponent_grid[sunk_index[0]][sunk_index[1]]}!")
         elif result == "hit":
             print("Congratulations! You hit an opponent's ship!")
         else:
             print("You missed! Try again.")
-            user_grid[guess_row][guess_col] = '*'
+            opponent_grid[guess_row][guess_col] = '*'
 
         print("\nYour Board:")
-        print_user_board(user_grid, size)
+        print_board(user_grid, size)
 
         print("\nOpponent's Board:")
-        for row, col in opponent_guesses:
-            opponent_grid[row][col] = 'X' if (row, col) in user_ships[sunk_index] else '*'
-        print_user_board(opponent_grid, size)
+        print_board(opponent_grid, size)
 
-        if not opponent_ships:
+        if not any('O' in row for row in opponent_grid):
             print("Congratulations! You've sunk all the opponent's ships!")
             break
 
         print(f"\nAttempt {attempt + 1}: Opponent's Turn")
-        opponent_guess_row = random.randint(0, size - 1)
-        opponent_guess_col = random.randint(0, size - 1)
-        opponent_guess = (opponent_guess_row, opponent_guess_col)
+        computer_guess_location = computer_guess(user_ships, size)
+        computer_guess_result, _ = check_hit_and_sunk(computer_guess_location, user_grid)
 
-        opponent_guesses.append(opponent_guess)
-
-        result, _ = check_hit_and_sunk(opponent_guess, user_ships)
-
-        if result == "sunk":
-            print("The opponent hit and sunk your ship!")
-        elif result == "hit":
-            print("The opponent hit your ship!")
+        if computer_guess_result == "sunk":
+            print(f"The opponent hit and sunk your ship at {computer_guess_location}!")
+        elif computer_guess_result == "hit":
+            print(f"The opponent hit your ship at {computer_guess_location}!")
         else:
-            print("The opponent missed!")
+            print(f"The opponent missed at {computer_guess_location}!")
+
+        opponent_guesses.append(computer_guess_location)
 
     else:
         print("Game over! You've used all your attempts.")
         print("The remaining opponent's ships:")
-        for ship in opponent_ships:
-            print(f"{ship[0][0]}")
+        for i, row in enumerate(opponent_grid):
+            for j, col in enumerate(row):
+                if col != 'O':
+                    print(f"{col} at ({i+1}, {j+1})")
 
     play_again_input = input("Do you want to play again? (yes/no): ").lower()
     if play_again_input == 'yes':
